e33cdfda397d216a502d97309f77975854c1281a
hehongjie <hehj@koal.com>
2017-10-29 19:09:45 +0800

    mac

---
+++ indent/custom/custom.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -sf ~/.dotfiles/indent/custom/.indent.pro.kr.style ~/.indent.pro
+cp -a ~/.dotfiles/indent/custom/indent_custom /usr/local/bin/
---
+++ spf13-vim/custom/custom.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.local ~/.vimrc.local
+ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.bundles.local ~/.vimrc.bundles.local
