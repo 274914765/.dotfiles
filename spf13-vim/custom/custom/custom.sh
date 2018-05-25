969c5f178290139fde15d88ddf7584e9d38ea5ef
hehongjie <hehj@koal.com>
2018-05-25 20:29:09 +0800

    rename files

--- spf13-vim/custom/custom/custom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.local ~/
-ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.before.local ~/
-ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.bundles.fork ~/
-
