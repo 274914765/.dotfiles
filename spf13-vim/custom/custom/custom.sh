9f5e81da77416fb70da0014f331b53c272b9470d
hehongjie <hehj@koal.com>
2018-05-25 16:26:16 +0800

    custom spf13-vim

---
+++ spf13-vim/custom/custom/custom.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.local ~/
+ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.before.local ~/
+ln -sf ~/.dotfiles/spf13-vim/custom/.vimrc.bundles.fork ~/
+
