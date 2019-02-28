ec38b62c1f4df96ddeeebfd058e37503e54a520f
hehongjie <hehj@koal.com>
2019-02-28 15:43:11 +0800

    ssl-dev

---
+++ Oh-My-Zsh/custom/custom.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf ~/.zshrc
+ln -s ~/.dotfiles/Oh-My-Zsh/custom/.zshrc ~/.zshrc
---
+++ autojump/custom/custom.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ln -s ~/.dotfiles/autojump/custom/autojump.zsh /etc/profile.d/autojump.zsh
